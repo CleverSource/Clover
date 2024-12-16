1536db2cd59974686f80fc9912e94188207e821d
CleverSource <stunthound@gmail.com>
2024-12-16 09:52:05 -0500

    Initial commit

---
+++ testbed/build.bat
@@ -0,0 +1,22 @@
+REM Build script for testbed
+@ECHO OFF
+SetLocal EnableDelayedExpansion
+
+REM Get a list of all the .c files.
+SET cFilenames=
+FOR /R %%f in (*.c) do (
+    SET cFilenames=!cFilenames! %%f
+)
+
+SET assembly=testbed
+SET compilerFlags=-g
+SET includeFlags=-Isrc -I../engine/src
+SET linkerFlags=-L../bin/ -lengine.lib
+SET defines=-D_DEBUG -DCIMPORT
+
+ECHO "Building %assembly%%..."
+clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.exe %defines% %includeFlags% %linkerFlags%PUSHD engine
+CALL build.bat
+POPD
+IF %ERRORLEVEL% NEQ 0 (echo Error:%errorlevel% && exit)
+
