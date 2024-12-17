ca9c9f966df1d1bf3d3b927cad037fe40ebb4150
CleverSource <stunthound@gmail.com>
2024-12-16 19:04:05 -0500

    Input & events working for Win32

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -21,6 +21,9 @@ typedef struct application_state {
 static b8 initialized = FALSE;
 static application_state app_state;
 
+b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context);
+b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context);
+
 b8 application_create(game* game_inst)
 {
     if (initialized) {
@@ -49,6 +52,10 @@ b8 application_create(game* game_inst)
         CERROR("Event system failed initialization. Application cannot continue");
         return FALSE;
     }
+
+    event_register(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+    event_register(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+    event_register(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
 
     if (!platform_startup(
         &app_state.platform, 
@@ -103,9 +110,52 @@ b8 application_run()
 
     app_state.is_running = FALSE;
 
+    event_unregister(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+    event_unregister(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+    event_unregister(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
     event_shutdown();
+    input_shutdown();
 
     platform_shutdown(&app_state.platform);
 
     return TRUE;
+}
+
+b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context)
+{
+    switch (code) {
+        case EVENT_CODE_APPLICATION_QUIT: {
+            CINFO("EVENT_CODE_APPLICATION_QUIT received, shutting down");
+            app_state.is_running = FALSE;
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context)
+{
+    if (code == EVENT_CODE_KEY_PRESSED) {
+        u16 key_code = context.data.u16[0];
+        if (key_code == KEY_ESCAPE) {
+            event_context data = {};
+            event_fire(EVENT_CODE_APPLICATION_QUIT, 0, data);
+
+            return TRUE;
+        } else if (key_code == KEY_A) {
+            CDEBUG("Explicit - A key pressed");
+        } else {
+            CDEBUG("'%c' key pressed", key_code);
+        }
+    } else if (code == EVENT_CODE_KEY_RELEASED) {
+        u16 key_code = context.data.u16[0];
+        if (key_code == KEY_B) {
+            CDEBUG("Explicit - B key released");
+        } else {
+            CDEBUG("'%c' key released", key_code);
+        }
+    }
+
+    return FALSE;
 }
