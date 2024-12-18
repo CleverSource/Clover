3da9b4b285cbadd364f518a4a955c2a0cf2a8de6
CleverSource <stunthound@gmail.com>
2024-12-17 23:28:01 -0500

    Clearing to screen

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -27,6 +27,7 @@ static application_state app_state;
 
 b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context);
 b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context);
+b8 application_on_resized(u16 code, void* sender, void* listener_inst, event_context context);
 
 b8 application_create(game* game_inst)
 {
@@ -60,6 +61,7 @@ b8 application_create(game* game_inst)
     event_register(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
     event_register(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
     event_register(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
+    event_register(EVENT_CODE_RESIZED, 0, application_on_resized);
 
     if (!platform_startup(
         &app_state.platform, 
@@ -207,6 +209,36 @@ b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context
             CDEBUG("Explicit - B key released");
         } else {
             CDEBUG("'%c' key released", key_code);
+        }
+    }
+
+    return FALSE;
+}
+
+b8 application_on_resized(u16 code, void* sender, void* listener_inst, event_context context)
+{
+    if (code == EVENT_CODE_RESIZED) {
+        u16 width = context.data.u16[0];
+        u16 height = context.data.u16[1];
+
+        if (width != app_state.width || height != app_state.height) {
+            app_state.width = width;
+            app_state.height = height;
+
+            CDEBUG("Window resize: %i, %i", width, height);
+
+            if (width == 0 || height == 0) {
+                CINFO("Window minimized, suspending application");
+                app_state.is_suspended = TRUE;
+                return TRUE;
+            } else {
+                if (app_state.is_suspended) {
+                    CINFO("Window restored, resuming application");
+                    app_state.is_suspended = FALSE;
+                }
+                app_state.game_inst->on_resize(app_state.game_inst, width, height);
+                renderer_on_resized(width, height);
+            }
         }
     }
 
